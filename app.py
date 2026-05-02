bee943bf2f9475fc8ecdd39850187277b8656e42
Bruno Freitas <brunonfreitas72@gmail.com>
2026-05-02 14:00:46 +0100

    dynamic chapters loading

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+def load_chapters():
+    folder = "study_notes"
+    chapters = []
+
+    for file in os.listdir(folder):
+        if file.endswith(".md"):
+            with open(os.path.join(folder, file), "r", encoding="utf-8") as f:
+                chapters.append(f.read())
+
+    return chapters
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    chapters = load_chapters()
+    return render_template("index.html", chapters=chapters)
 
-# IMPORTANTE para o Render
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
